0abbd244ec742ab480ab2b0380d91d8f75ec8d20
finlaywashere <finman292004@protonmail.com>
2021-01-16 15:50:06 -0500

    Removed all the locks to prevent any funky buisness

--- src/xyz/finlaym/adminbot/storage/config/SwearsConfig.java
+++ src/xyz/finlaym/adminbot/storage/config/SwearsConfig.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +16,7 @@ public class SwearsConfig {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SwearsConfig.class);
 	private DBInterface dbInterface;
-	
-	private Lock swearLock = new ReentrantLock();
+
 	private Map<Long,List<SwearWord>> swearWords = new HashMap<Long,List<SwearWord>>();
 	
 	public SwearsConfig(DBInterface dbInterface) {
@@ -29,32 +26,24 @@ public class SwearsConfig {
 		return swearWords.get(id);
 	}
 	public void addSwear(SwearWord s, long guildId){
-		swearLock.lock();
 		if(!swearWords.containsKey(guildId))
 			swearWords.put(guildId, new ArrayList<SwearWord>());
 		swearWords.get(guildId).add(s);
-		swearLock.unlock();
 	}
 	public void removeSwear(SwearWord s, long guildId) {
-		swearLock.lock();
 		if(!swearWords.containsKey(guildId))
 			return;
 		swearWords.get(guildId).remove(s);
-		swearLock.unlock();
 	}
 	public void loadSwears(long guildId) throws Exception {
-		swearLock.lock();
 		if(swearWords.containsKey(guildId))
 			swearWords.put(guildId, new ArrayList<SwearWord>());
 		dbInterface.getSwears(guildId, this);
-		swearLock.unlock();
 	}
 	public void saveSwears(long guildId) throws Exception {
-		swearLock.lock();
 		if(!swearWords.containsKey(guildId))
 			return;
 		dbInterface.saveSwears(guildId, this);
-		swearLock.unlock();
 	}
 	
 	public SwearWord isSwear(long guildId, String text, ActivationType type) {
@@ -62,7 +51,7 @@ public class SwearsConfig {
 			try {
 				loadSwears(guildId);
 			} catch (Exception e) {
-				logger.error("Pogger, ewwor werror in isSwear", e.getCause());
+				logger.error("Poggers, ewwor werror in isSwear", e.getCause());
 			}
 		}
 		List<SwearWord> swears = swearWords.get(guildId);
--- src/xyz/finlaym/adminbot/storage/config/UserLevelConfig.java
+++ src/xyz/finlaym/adminbot/storage/config/UserLevelConfig.java
@@ -2,14 +2,11 @@ package xyz.finlaym.adminbot.storage.config;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import xyz.finlaym.adminbot.storage.DBInterface;
 
 public class UserLevelConfig {
 	private Map<Long,Integer> levels;
-	private Lock levelsLock = new ReentrantLock();
 	private DBInterface dbInterface;
 	
 	public UserLevelConfig(DBInterface dbInterface) {
@@ -17,28 +14,19 @@ public class UserLevelConfig {
 		this.levels = new HashMap<Long,Integer>();
 	}
 	public void loadLevels(long id) throws Exception {
-		levelsLock.lock();
 		dbInterface.loadUserLevels(id,this);
-		levelsLock.unlock();
 	}
 	public void saveLevels(long id) throws Exception {
-		levelsLock.lock();
 		dbInterface.saveUserLevels(id, this);
-		levelsLock.unlock();
 	}
 	public int getUserLevels(long id) {
-		levelsLock.lock();
 		if(!levels.containsKey(id)) {
-			levelsLock.unlock();
 			return 0;
 		}
 		int level = levels.get(id);
-		levelsLock.unlock();
 		return level;
 	}
 	public void setUserLevels(long id, int level) {
-		levelsLock.lock();
 		levels.put(id, level);
-		levelsLock.unlock();
 	}
 }
